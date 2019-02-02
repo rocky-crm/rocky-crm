c71b2e92108f2d208b3c5ce0b545f2f2735c874e
dave-yang <daworld.ny@gmail.com>
2019-02-02 20:46:59 +0500

    Spending controller and its routes

--- module/Finance/Config/routes.php
+++ module/Finance/Config/routes.php
@@ -1,4 +1,19 @@
 <?php
 
 return [
+    '/finance/spending' => [
+        'controller' => 'Spending@indexAction'
+    ],
+
+    '/finance/spending/save' => [
+        'controller' => 'Spending@saveAction'
+    ],
+
+    '/finance/spending/edit/(:var)' => [
+        'controller' => 'Spending@editAction'
+    ],
+
+    '/finance/spending/delete/(:var)' => [
+        'controller' => 'Spending@deleteAction'
+    ]
 ];
