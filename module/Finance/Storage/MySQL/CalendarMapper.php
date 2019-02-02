10d68a1ff69fdded88e2ef742cf2f43265d0cf7c
dave-yang <daworld.ny@gmail.com>
2019-02-02 21:52:04 +0500

    Added fetchAll()

--- module/Finance/Storage/MySQL/CalendarMapper.php
+++ module/Finance/Storage/MySQL/CalendarMapper.php
@@ -21,4 +21,40 @@ final class CalendarMapper extends AbstractMapper
     {
         return 'id';
     }
+
+    /**
+     * Returns all calendar entries
+     * 
+     * @param string $date Optional date constraint
+     * @return array
+     */
+    public function fetchAll($date) : array
+    {
+        // Columns to be selected
+        $columns = [
+            self::column('id'),
+            self::column('spending_id'),
+            SpendingMapper::column('name') => 'spending',
+            self::column('date'),
+            self::column('name'),
+            self::column('amount'),
+        ];
+
+        $db = $this->db->select($columns)
+                       ->from(self::getTableName())
+                       // Spending relation
+                       ->leftJoin(SpendingMapper::getTableName(), [
+                            SpendingMapper::column('id') => self::getRawColumn('spending_id')
+                       ]);
+
+        // Apply if required
+        if ($date !== null) {
+            $db->whereEquals(self::column('date'), $date);
+        }
+
+        $db->orderBy($this->getPk())
+           ->desc();
+
+        return $db->queryAll();
+    }
 }
